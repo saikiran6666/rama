009246469efa70c736b7a9efbb1e865635f84ad8
s <manikishorepuppala@gmail.com>
2019-10-14 10:58:38 +0530

    hihihi

---
+++ operators.py
@@ -0,0 +1,164 @@
+#addition
+x=12
+y=2
+print(x+y)
+#substraction
+x=10
+y=5
+print(x-y)
+
+#multiplication
+x=10
+y=10
+print(x*y)
+
+#division
+x=10
+y=2
+print(x / y)
+
+#modulus
+x=6
+y=2
+print(x%y)
+
+#exponenetiation
+x=2
+y=3
+print(x**y)
+
+#floor division
+x=5
+y=2
+print(x // y)
+
+#assignment operators
+# "=" operator
+x=10
+print(x)
+# += operator
+x=10
+x+=2
+print(x)
+
+# (-=) operator
+x=10
+x-=2
+print(x)
+
+# *= operator
+x=10
+x*=2
+print(x)
+
+# /= operator
+x=10
+x/=2
+print(x)
+
+# %= operator
+x=10
+x%=2
+print(x)
+
+# //= operator
+x=10
+x//=3
+print(x)
+
+# **= operator
+x=10
+x**=3
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#>>= operator
+x=5
+x>>=3
+print(x)
+
+#comparision operator
+# == operator
+x=5
+y=3
+print(x==y)
+
+#!= operator
+x=5
+y=3
+print(x!=y)
+
+#> graterthan operator
+x=5
+y=3
+print(x>y)
+
+#< lessthan operator
+x=5
+y=3
+print(x<y)
+
+#>= graterthan or equal to operator
+x=5
+y=3
+print(x>=y)
+
+#<= graterthan or equal to operator
+x=5
+y=3
+print(x<=y)
+
+#logical oprators
+
+#and operator
+x=5
+print(x>3 and x<10)
+
+# or operator
+x=5
+print(x>3 or x<4)
+
+# not operator
+x=5
+print(not(x>3 or x<10))
+
+# identity operators
+# is oprator
+x= ["apple , banana"]
+y= ["apple , banana"]
+z=x
+print (x is z)
+
+print(x is y)
+
+print(x==y)
+
+# is not operator
+x= ["apple , banana"]
+y= ["apple , banana"]
+z=x
+print (x is not z)
+
+print(x is not y)
+
+print(x==y)
+
+#membership operators
+# in operator
+x= ["apple","banana"]
+print( "apple"  in x)
+# not in operator
+x= ["apple","banana"]
+print("pineapple"not in x)
