be556f6925c82efad0ded5e7bee4fb65a4ea1688
s <manikishorepuppala@gmail.com>
2019-10-14 12:31:27 +0530

    rama file

---
+++ program9.py
@@ -0,0 +1,18 @@
+#change all the items into uppercase
+# l="mani kishore and sai kiaran both are brother"
+# x=l.upper()
+# print(x)
+
+# lines="mani"
+# while True:
+#     s=lines
+#     if s:
+#         s.upper()
+#     else:
+#         break
+
+while True:
+    a=input()
+    if a:
+        break
+print(a.upper())
