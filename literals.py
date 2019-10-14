2945e47484e4d7ef44c373dfb4db0115fd4f931f
s <manikishorepuppala@gmail.com>
2019-10-14 10:17:15 +0530

    this is literals file

---
+++ literals.py
@@ -0,0 +1,12 @@
+a=0b1010#binary literals
+b=100#desimal literals
+c=0o310#0ctal literals
+d=0x12c#hexadesimal
+#flaoat literals
+float_1=10.5
+float_2=1.5e2
+#complex literal
+x= 1.34j
+print(a , b ,c ,d)
+print(float_1,float_2)
+print(x,x.img,x.real)
